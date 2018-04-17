5d3872520bf8ecf937406bf3315745a7c1373bc4
mayurkhatri <mayurkhatri122@gmail.com>
2018-04-17 19:00:59 +0530

    Add sidebar on home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.all
+
+users.each do |user|
+  if user.albums.blank?
+    user.albums.create(name: "Mobile Uploads")
+    user.albums.create(name: "Timeline Posts")
+  end
+end
