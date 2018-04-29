b1353a2b7cb018530d17576f856b0b31214f2fdf
mayurkhatri <mayurkhatri122@gmail.com>
2018-04-29 19:03:03 +0530

    Model specs - User - test associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe "validations" do
+    #it { should validate_presence_of(:title) }
+    it { should have_many(:albums) }
+    it { should have_many(:friendships) }
+    it { should have_many(:events) }
+    it { should have_many(:posts).through(:albums)}
+    it { should have_many(:friends).through(:friendships) }
+  end
+
   describe "#create_default_album" do
     it "is a success" do
       user = User.first
       expect{user.create_default_albums}.to change(Album, :count).by(2)
     end
-  end
-
-  describe "validations" do
-    #it { should validate_presence_of(:title) }
   end
 end
