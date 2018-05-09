f58aadb8ad8cb551d901a6282fc96f2585fcee64
mayurkhatri <mayurkhatri122@gmail.com>
2018-05-09 20:01:17 +0530

    Factories - include the parent models (consider belongs_to association)

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,18 @@
 FactoryBot.define do
   factory :post do
+    album
   end
   factory :photo do
+    post
+    image Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/fixtures/images/test.png')))
+    after :create do |b|
+      b.update_column(:image, "spec/fixtures/images/test.png")
+    end
   end
   factory :friendship do
   end
   factory :album do
+    user
   end
   factory :event do
     sequence(:name) { |n| "test#{n} event"}
