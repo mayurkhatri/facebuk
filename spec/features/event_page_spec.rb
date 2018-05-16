73554469c46466cd6c5fca7c2ef90d4910a83308
mayurkhatri <mayurkhatri122@gmail.com>
2018-05-16 18:20:37 +0530

    Sidebar Events link - feature spec / functional testing

---
+++ spec/features/event_page_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature 'events page' do
+  scenario 'click on events link' do
+    user = FactoryBot.create(:user)
+    login_as(user, :scope => :user)
+    visit('/')
+    # find('a', text: 'Events', exact: true)
+    click_link 'Events'
+    expect(page).to have_selector('h1', exact_text: 'Events')
+  end
+end
